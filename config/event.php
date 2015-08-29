f7c81421b4b4b499e2941a17023a9cd246c7ab99
Olivier Laviale <olivier.laviale@gmail.com>
2015-08-29 22:26:32 +0200

    New operations namspace

--- config/event.php
+++ config/event.php
@@ -10,7 +10,7 @@ $hooks = Hooks::class . '::';
 return [
 
 	Icybee\Modules\Pages\EditBlock::class . '::alter_children' => $hooks . 'on_page_editblock_alter_children',
-	Icybee\Modules\Pages\ExportOperation::class . '::process' => $hooks . 'on_operation_export',
+	Icybee\Modules\Pages\Operation\ExportOperation::class . '::process' => $hooks . 'on_operation_export',
 	Icybee\Modules\Pages\PageRenderer::class . '::render' => $hooks . 'on_page_renderer_render',
 	Icybee\Modules\Sites\EditBlock::class . '::alter_children' => $hooks . 'on_site_editblock_alter_children',
 	Brickrouge\Document::class . '::render_title:before' => $hooks . 'before_document_render_title',
